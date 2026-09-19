eae8ad645ea75bdb26618a2eb4460a2a170b6955
W3C CT <web3currency.info@gmail.com>
2026-09-19 14:15:50 +0100

    Remove standalone service routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,6 @@ import { ErrorBoundary } from "@/components/shared/error-boundary";
 import Home from "@/pages/home";
 import About from "@/pages/about";
 import Services from "@/pages/services";
-import DigitalConsulting from "@/pages/services/digital-consulting";
-import CryptoP2P from "@/pages/services/crypto-p2p";
-import WebDevelopment from "@/pages/services/web-development";
-import CommunityService from "@/pages/services/community";
 import Projects from "@/pages/projects";
 import ProjectDetail from "@/pages/project-detail";
 import Contact from "@/pages/contact";
@@ -53,10 +49,6 @@ function Router() {
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/services" component={Services} />
-        <Route path="/services/digital-consulting" component={DigitalConsulting} />
-        <Route path="/services/crypto-p2p" component={CryptoP2P} />
-        <Route path="/services/web-development" component={WebDevelopment} />
-        <Route path="/services/community" component={CommunityService} />
         <Route path="/projects" component={Projects} />
         <Route path="/projects/:id" component={ProjectDetail} />
         <Route path="/community" component={CommunityRedirect} />
