bc88f081d0989c6a17847bece25d83cc7f0f4374
W3C CT <web3currency.info@gmail.com>
2026-09-19 09:48:54 +0100

    Add W3C Pi Bookings project preview

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -35,6 +35,8 @@ const getProjectImage = (id: string) => {
       return "https://i.imgur.com/qO5yasK.jpeg";
     case "w3c-test-token-tracker":
       return "https://i.imgur.com/oFxxGhs.jpeg";
+    case "w3c-pi-bookings":
+      return "https://i.imgur.com/UofCtfk.jpeg";
     case "ceecar":
       return "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?auto=format&fit=crop&w=800&q=80";
     default:
